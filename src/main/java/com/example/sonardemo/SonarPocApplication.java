34e14ae12020203e903eef5cd8081b84345c1589
Robinson Correa <rcorrea@vumigroup.com>
2026-04-08 16:48:24 -0500

    add_loog

--- src/main/java/com/example/sonardemo/SonarPocApplication.java
+++ src/main/java/com/example/sonardemo/SonarPocApplication.java
@@ -2,14 +2,17 @@ package com.example.sonardemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class SonarPocApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SonarPocApplication.class, args);
-		System.out.println("Hello, SonarQube!");
-		
+		Logger logger = LoggerFactory.getLogger(SonarPocApplication.class);
+		logger.info("Hello, SonarQube!");
+
 	}
 
 }
