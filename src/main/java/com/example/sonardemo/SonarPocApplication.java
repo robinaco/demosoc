83bb0a1beaa134b669965b0acf6899e1b523d393
Robinson Correa <rcorrea@vumigroup.com>
2026-04-08 17:19:54 -0500

    add_loog567

--- src/main/java/com/example/sonardemo/SonarPocApplication.java
+++ src/main/java/com/example/sonardemo/SonarPocApplication.java
@@ -2,13 +2,11 @@ package com.example.sonardemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import jakarta.annotation.PostConstruct;
+
 
 @SpringBootApplication
 public class SonarPocApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SonarPocApplication.class, args);
 	}
