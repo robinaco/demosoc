da08c734012707fe478006874add1141b2235da8
Robinson Correa <rcorrea@vumigroup.com>
2026-04-08 20:40:20 -0500

    fix: move PersonaDTOTest to correct package

--- src/main/java/com/example/dto/PersonaDTO.java
+++ src/main/java/com/example/dto/PersonaDTO.java
@@ -1,8 +1,13 @@
 package com.example.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class PersonaDTO {
     private Long id;
     private String nombre;
